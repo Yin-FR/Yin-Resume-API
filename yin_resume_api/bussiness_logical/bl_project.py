e078a02ee9482e79f0d3d655b61db38fab115210
Yin-FR <tanyin625@gmail.com>
2023-10-08 19:54:57 +0200

    add token in git repo operations

--- yin_resume_api/bussiness_logical/bl_project.py
+++ yin_resume_api/bussiness_logical/bl_project.py
@@ -1,15 +1,15 @@
-from yin_resume_api.utils.github_operation.git_repo import GithubRepoController
+from yin_resume_api.utils.github_operation import grc
 from yin_resume_api.utils.api import get_data_from_url
 from flask import jsonify
 
 
 def bl_get_all_repos(username):
-    grc = GithubRepoController(username)
     repos = grc.get_all_repo()
+    languages = grc.get_language_repo()
     result = []
     for each_repo in repos:
         title = " ".join([word.capitalize() for word in each_repo["name"].split("-")])
-        language = get_data_from_url(each_repo["languages_url"])[1]
+        language = languages[each_repo["name"]]
         if language:
             main_language = max(language, key=language.get)
         else:
